2b2e668b9a4d8ae700daa76a60e0edda261332f1
cristiansep <cristiansepulvedamendez@gmail.com>
2020-06-29 13:20:32 -0400

    docs agregado

---
+++ docs/precache-manifest.d0c6ccf3756add9113aacba894af5fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f258d33c5a5134c180c8ed2f67c318f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1adb18f697a2416f04ed",
+    "url": "/static/css/main.d7516bac.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adb18f697a2416f04ed",
+    "url": "/static/js/main.424f3e9d.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
